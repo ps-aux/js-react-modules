d42e6b6bcebcca09799b2ab65414e563ff5e4292
ps-aux <seriouz.accz@gmail.com>
2018-06-03 02:42:10 +0200

    Add showInfo Storybook addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react'
 import 'babel-plugin-external-helpers'
+import { withInfo } from '@storybook/addon-info'
+import { addDecorator } from '@storybook/react'
 
 function loadStories () {
+    addDecorator((story, context) => withInfo('common info')(story)(context))
     require('../src/stories')
 }
 
